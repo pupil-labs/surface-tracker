f90d04f437e56737db694551d11ae9bf8e2eaab4
Roman Roibu <romanroibu@gmail.com>
2020-02-11 18:46:54 +0100

    Add (un)distort_points_to_image_plane methods to CameraModel

--- src/surface_tracker/camera.py
+++ src/surface_tracker/camera.py
@@ -1,5 +1,12 @@
+import abc
 import typing as T
 
 
-class CameraModel:
-    pass
+class CameraModel(abc.ABC):
+    @abc.abstractmethod
+    def distort_points_on_image_plane(self, points: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def undistort_points_on_image_plane(self, points: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
