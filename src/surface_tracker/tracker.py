52064f579762b71bc2183875455f2d0106440909
Roman Roibu <romanroibu@gmail.com>
2020-02-13 17:29:20 +0100

    Update SurfaceTracker to use internal API to provide public functionality

--- src/surface_tracker/tracker.py
+++ src/surface_tracker/tracker.py
@@ -1,18 +1,37 @@
+import collections
+import logging
 import typing as T
 
+import numpy as np
+
 from .camera import CameraModel
+from .corner import CornerId
 from .marker import Marker, MarkerId
 from .location import SurfaceLocation
 from .surface import Surface, SurfaceId
+from .visual_anchors import SurfaceVisualAnchors
+
+
+logger = logging.getLogger(__name__)
 
 
 class SurfaceTracker:
-    def create_surface(
+
+    def __init__(self):
+        pass
+
+    def define_surface(
         self, name: str, markers: T.List[Marker], camera_model: CameraModel
-    ) -> Surface:
-        raise NotImplementedError()
+    ) -> T.Optional[Surface]:
+        return Surface._create_surface_from_markers(name=name, markers=markers, camera_model=camera_model)
 
     def locate_surface(
         self, surface: Surface, markers: T.List[Marker], camera_model: CameraModel
     ) -> T.Optional[SurfaceLocation]:
-        raise NotImplementedError()
+        """Computes a SurfaceLocation based on a list of visible markers
+        """
+        return SurfaceLocation._create_location_from_markers(
+            surface=surface,
+            markers=markers,
+            camera_model=camera_model,
+        )
