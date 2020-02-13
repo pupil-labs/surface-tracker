6fcaafa570e5ed3f7f606589eee5738c89bc8396
Roman Roibu <romanroibu@gmail.com>
2020-02-13 17:26:21 +0100

    Refine Surface interface

--- src/surface_tracker/surface.py
+++ src/surface_tracker/surface.py
@@ -2,8 +2,13 @@ import abc
 import enum
 import typing as T
 
+import numpy as np
+import cv2
+
+from .camera import CameraModel
 from .corner import CornerId
-from .marker import Marker, MarkerId
+from .marker import Marker, MarkerId, _MarkerInSurfaceSpace
+from .utils import bounding_quadrangle
 
 
 SurfaceId = T.NewType("SurfaceId", str)
@@ -12,6 +17,9 @@ SurfaceId = T.NewType("SurfaceId", str)
 class Surface(abc.ABC):
 
     ### Info
+    ### Abstract members
+
+    version = None  # type: ClassVar[int]
 
     @property
     @abc.abstractmethod
@@ -25,31 +33,47 @@ class Surface(abc.ABC):
 
     @property
     @abc.abstractmethod
-    def version(self) -> int:
+    def registered_markers_by_uid_distorted(self) -> T.Mapping[MarkerId, Marker]:
         raise NotImplementedError()
+
+    @property
+    @abc.abstractmethod
+    def registered_markers_by_uid_undistorted(self) -> T.Mapping[MarkerId, Marker]:
+        raise NotImplementedError()
+
+    @property
+    def registered_marker_uids(self) -> T.Set[MarkerId]:
+        marker_uids_distorted = set(self.registered_markers_by_uid_distorted.keys())
+        marker_uids_undistorted = set(self.registered_markers_by_uid_undistorted.keys())
+        marker_uids_diff = marker_uids_distorted.symmetric_difference(marker_uids_undistorted)
+
+        for uid in marker_uids_diff:
+            logger.debug(f"Removing inconsistently registered marker with UID: {uid}")
+            del self.registered_markers_by_uid_distorted[uid]
+            del self.registered_markers_by_uid_undistorted[uid]
+
+        return set(self.registered_markers_by_uid_distorted.keys())
 
     ### Update
 
-    @abc.abstractmethod
     def add_marker(self, marker: Marker):
         raise NotImplementedError()
 
-    @abc.abstractmethod
     def remove_marker(self, marker_uid: MarkerId):
         raise NotImplementedError()
 
-    @abc.abstractmethod
     def move_corner(self, corner_uid: CornerId, new_position):
         # TODO: Type annotate new_position
         raise NotImplementedError()
 
     ### Serialize
 
+    @abc.abstractmethod
+    def as_dict(self) -> dict:
+        raise NotImplementedError()
+
     @staticmethod
     @abc.abstractmethod
     def from_dict(value: dict) -> "Surface":
-        raise NotImplementedError()
-
-    @abc.abstractmethod
     def as_dict(self) -> dict:
         raise NotImplementedError()
