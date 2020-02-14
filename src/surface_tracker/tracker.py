45aedd2544a17adf4df0653810e212ed9b5db8db
Roman Roibu <romanroibu@gmail.com>
2020-02-14 14:40:49 +0100

    Refine and extend the SurfaceTracker interface

--- src/surface_tracker/tracker.py
+++ src/surface_tracker/tracker.py
@@ -16,28 +16,58 @@ logger = logging.getLogger(__name__)
 
 
 class SurfaceTracker:
-    def __init__(self):
-        pass
+    def __init__(self, camera_model: CameraModel):
+        self.__camera_model = camera_model
 
-    def define_surface(
-        self, name: str, markers: T.List[Marker], camera_model: CameraModel
-    ) -> T.Optional[Surface]:
+    ### Creating a surface
+
+    def define_surface(self, name: str, markers: T.List[Marker]) -> T.Optional[Surface]:
         return Surface._create_surface_from_markers(
-            name=name, markers=markers, camera_model=camera_model
+            name=name, markers=markers, camera_model=self.__camera_model
         )
 
+    ### Inspecting a surface
+
+    def surface_corner_position_in_image_space(
+        self, surface: Surface, location: SurfaceLocation, corner: CornerId
+    ) -> T.Tuple[int, int]:
+        raise NotImplementedError()
+
+    ### Modifying a surface
+
+    def move_surface_corner_position_in_image_space(
+        self,
+        surface: Surface,
+        location: SurfaceLocation,
+        corner: CornerId,
+        new_position: T.Tuple[int, int],
+    ):
+        raise NotImplementedError()
+
+    def add_marker_to_surface(
+        self, surface: Surface, location: SurfaceLocation, marker: Marker
+    ):
+        raise NotImplementedError()
+
+    def remove_marker_from_surface(
+        self, surface: Surface, location: SurfaceLocation, marker_uid: MarkerId
+    ):
+        raise NotImplementedError()
+
+    ### Locating a surface
+
     def locate_surface(
-        self, surface: Surface, markers: T.List[Marker], camera_model: CameraModel
+        self, surface: Surface, markers: T.List[Marker]
     ) -> T.Optional[SurfaceLocation]:
         """Computes a SurfaceLocation based on a list of visible markers
         """
         return SurfaceLocation._create_location_from_markers(
-            surface=surface, markers=markers, camera_model=camera_model
+            surface=surface, markers=markers, camera_model=self.__camera_model
         )
 
-    def locate_visual_anchors(
-        self, location: SurfaceLocation, camera_model
-    ) -> SurfaceVisualAnchors:
+    def locate_surface_visual_anchors(
+        self, surface: Surface, location: SurfaceLocation
+    ) -> T.Optional[SurfaceVisualAnchors]:
         return SurfaceVisualAnchors._create_from_location(
-            location=location, camera_model=camera_model
+            location=location, camera_model=self.__camera_model
         )
