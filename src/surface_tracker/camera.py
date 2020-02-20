0dd2e2175e2e70680a39187617c26a1e2899f050
Roman Roibu <romanroibu@gmail.com>
2020-02-20 11:13:36 +0100

    Add requirement to CameraModel to implement resolution property

--- src/surface_tracker/camera.py
+++ src/surface_tracker/camera.py
@@ -5,6 +5,14 @@ import numpy as np
 
 
 class CameraModel(abc.ABC):
+
+    @property
+    @abc.abstractmethod
+    def resolution(self) -> T.Tuple[int, int]:
+        """Returns `(width, height)`
+        """
+        raise NotImplementedError()
+
     @abc.abstractmethod
     def distort_points_on_image_plane(self, points: np.ndarray) -> np.ndarray:
         raise NotImplementedError()
