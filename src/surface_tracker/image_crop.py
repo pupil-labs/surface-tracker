02bb6db5a7845b1d17d39d1e2f514ff490197f50
Roman Roibu <romanroibu@gmail.com>
2020-03-05 16:07:58 +0100

    Add missing license header to source files

--- src/surface_tracker/image_crop.py
+++ src/surface_tracker/image_crop.py
@@ -1,3 +1,12 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2020 Pupil Labs
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See LICENSE for license details.
+---------------------------------------------------------------------------~(*)
+"""
 import typing as T
 
 import numpy as np
